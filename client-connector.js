3eb427caf53bedc0a8a917ef64a1c082b6dd84b0
akildevs1000 <akildevs1000@gmail.com>
2025-02-25 12:03:35 +0400

    test

--- client-connector.js
+++ client-connector.js
@@ -1,9 +1,7 @@
 const pm2 = require("pm2");
-const fs = require("fs");
-const path = require("path");
 
-const sessionBasePath = path.join(__dirname, ".wwebjs_auth");
-const oneDayAgo = Date.now() - 24 * 60 * 60 * 1000; // 24 hours ago
+const { clientIds } = require("./clients.json");
+
 
 pm2.connect((err) => {
     if (err) {
@@ -11,57 +9,28 @@ pm2.connect((err) => {
         process.exit(1);
     }
 
-    // Ensure the session directory exists
-    if (fs.existsSync(sessionBasePath)) {
-        const sessionFolders = fs.readdirSync(sessionBasePath).filter(folder => folder.startsWith("session-"));
+    clientIds.forEach(clientId => {
+        if (clientId === "AE00027") {
+            let payload = {
+                script: "client.js",
+                name: `child-process-${clientId}`,
+                autorestart: true,
+                watch: false,
+                args: [clientId],
+            };
 
-        sessionFolders.forEach(folder => {
-            const folderPath = path.join(sessionBasePath, folder);
+            console.log(payload);
 
-            try {
-                const stats = fs.statSync(folderPath);
-                const lastModified = stats.mtime.getTime();
-
-                if (lastModified > oneDayAgo) {
-                    if (folder.split("session-")[1]) {
-                        let clientId = folder.split("session-")[1];
-
-                        if (clientId == "test") {
-                            let payload = {
-                                script: "client.js",
-                                name: `child-process-${clientId}`,
-                                autorestart: true,
-                                max_restarts: 5,
-                                watch: false,
-                                args: [clientId],
-                            };
-
-                            console.log(payload);
-
-                            pm2.start(payload, (err, apps) => {
-                                if (err) {
-                                    console.error(`Error starting process for ${clientId}:`, err);
-                                    return;
-                                }
-
-                                console.log(`Child process started for ${clientId}`);
-                            }
-                            );
-                        }
-                    }
-
+            pm2.start(payload, (err, apps) => {
+                if (err) {
+                    console.error(`Error starting process for ${clientId}:`, err);
+                    return;
                 }
-            } catch (err) {
-                if (err.code !== "ENOENT") {
-                    console.log(`❌ Error processing folder ${folder}: ${err.message}`);
-                }
-            }
-        });
-    } else {
-        console.log("Session directory does not exist.");
-    }
 
-
+                console.log(`Child process started for ${clientId}`);
+            });
+        }
+    });
 
     // Disconnect from PM2 after starting all processes
     setTimeout(() => {
